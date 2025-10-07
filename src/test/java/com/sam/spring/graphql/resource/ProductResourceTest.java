ca44f4f2d287eb189fcbd090cb576ccad3a4f2dc
Sam <hashanrandika1996@gmail.com>
2025-10-07 11:21:28 +1100

    add few mutation query related test cases and update existing test cases

--- src/test/java/com/sam/spring/graphql/resource/ProductResourceTest.java
+++ src/test/java/com/sam/spring/graphql/resource/ProductResourceTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @GraphQlTest(ProductResource.class)
@@ -26,6 +26,7 @@ public class ProductResourceTest {
         List<Product> mockProducts = List.of(new Product(1, "Laptop", "Electronic", 2.45f, 10));
         when(productService.getProducts()).thenReturn(mockProducts);
         graphQlTester.documentName("product-query")
+                .operationName("getProducts")
                 .execute()
                 .path("getProducts")
                 .entityList(Product.class)
@@ -40,6 +41,7 @@ public class ProductResourceTest {
 
         when(productService.findByCategory(anyString())).thenReturn(mockProducts);
         graphQlTester.documentName("product-query")
+                .operationName("findProductByCategory")
                 .execute()
                 .path("findProductByCategory")
                 .entityList(Product.class)
@@ -51,9 +53,45 @@ public class ProductResourceTest {
         when(productService.findByCategory(anyString())).thenReturn(List.of());
 
         graphQlTester.documentName("product-query")
+                .operationName("findProductByCategory")
                 .execute()
                 .path("findProductByCategory")
                 .entityList(Product.class)
                 .hasSize(0);
+    }
+
+    @Test
+    void createProduct_shouldReturnNewProduct() {
+        Product product = new Product(1, "Laptop", "Electronic", 2.45f, 10);
+
+        when(productService.createProduct(anyString(), anyString(), anyFloat(), anyInt()))
+                .thenReturn(product);
+
+        graphQlTester.documentName("product-query")
+                .operationName("createProduct")
+                .execute()
+                .path("createProduct")
+                .entity(Product.class)
+                .path("createProduct.id").entity(Integer.class).isEqualTo(1)
+                .path("createProduct.name").entity(String.class).isEqualTo("Laptop")
+                .path("createProduct.category").entity(String.class).isEqualTo("Electronic")
+                .path("createProduct.price").entity(Float.class).isEqualTo(2.45f)
+                .path("createProduct.stock").entity(Integer.class).isEqualTo(10);
+    }
+
+    @Test
+    void updateStock_shouldReturnUpdateProduct() {
+        Product product = new Product(1, "Laptop", "Electronic", 2.45f, 25);
+
+        when(productService.updateStock(anyInt(), anyInt()))
+                .thenReturn(product);
+
+        graphQlTester.documentName("product-query")
+                .operationName("updateStock")
+                .execute()
+                .path("updateStock")
+                .entity(Product.class)
+                .path("updateStock.id").entity(Integer.class).isEqualTo(1)
+                .path("updateStock.stock").entity(Integer.class).isEqualTo(25);
     }
 }
